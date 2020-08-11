dac020e7518fdfac047012b69d1c9ef97700b441
BlkPingu <tobias-kolb@hotmail.de>
2020-08-11 13:29:31 +0200

    edit privacy acknoledgement

--- src/assets/privacyPolicy.js
+++ src/assets/privacyPolicy.js
@@ -2,9 +2,9 @@ module.exports = `
 
 <p>Ich stimmte zu, volljährig zu sein.</p>
 
-<p>Ich stimme der verarbeitung meiner Daten im Rahmen der Datenschutzerklärung in einklang mit der DSGVO zu.
+<p>Ich stimme der <a href="https://tobiaskolb.dev/#/privacy-notice">Datenschutzerklärung</a> zu.</p>
 
-<p>Ich stimme der <a href="https://tobiaskolb.dev/#/privacy">Datenschutzerklärung</a> zu.</p>
+<p>Ich stimme der Verarbeitung meiner Daten im Rahmen der Datenschutzerklärung und im Einklang mit der <a href="https://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX:32016L0680">DSGVO</a> zu.
 
 <p>Es werden folgende Daten erhoben:</p>
 
@@ -14,4 +14,6 @@ module.exports = `
   <li>Muttersprache</li>
   <li>Sprachbefehle Form von Stimm Stichprobem gesprochener Wörter oder Sätze</li>
 </ul>
+
+<p>Ich stimme zu, dass die so erhobenen Daten werden unter [Lizenz] zur Verfügung gestellt werden.</p>
 `
