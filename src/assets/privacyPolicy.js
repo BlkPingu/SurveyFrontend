5b02d8c21f3932027824b0018c6d1d91f86d6d18
BlkPingu <tobias-kolb@hotmail.de>
2020-08-20 02:06:50 +0200

    fix type in privacy notice

--- src/assets/privacyPolicy.js
+++ src/assets/privacyPolicy.js
@@ -1,5 +1,5 @@
 module.exports = `
-<p>Diese Befragung schließt Personen unter 16 Jahren aus.</p>
+<p>Diese Befragung schließt Personen unter 18 Jahren aus.</p>
 
 <p>Ich bestätige, dass Ich die <a href="https://tobiaskolb.dev/#/privacy-notice">Datenschutzerklärung</a> gelesen und verstanden habe und darüber hinaus keine Einwande habe.</p>
 `
